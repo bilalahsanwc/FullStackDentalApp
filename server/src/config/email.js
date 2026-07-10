bfa581c9ed9da374ae9adc1b65e2422414a5bf44
bilalahsanwc <hmbilal2024a@gmail.com>
2026-07-10 17:30:46 +0500

    debug transporter loading

--- server/src/config/email.js
+++ server/src/config/email.js
@@ -1,3 +1,4 @@
+
 import dotenv from "dotenv";
 dotenv.config();
 import nodemailer from "nodemailer";
@@ -6,14 +7,14 @@ console.log("🔥 TRANSPORTER FILE LOADED");
 console.log(process.env.EMAIL_USER);
 console.log(process.env.EMAIL_PASS);
 console.log(process.env.CLINIC_EMAIL);
+import dns from "dns";
+
+dns.setDefaultResultOrder("ipv4first");
+
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
-  family: 4,
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
